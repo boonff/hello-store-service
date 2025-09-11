8ca778076b1d8103a714a9519181f99d35a31332
boon <qxxpdada@outlook.com>
2025-09-11 18:09:58 +0800

    fix(SecurityConfig): 移除默认用户认证并允许所有请求访问

--- src/main/kotlin/com/hello/hello_store_service/SecurityConfig.kt
+++ src/main/kotlin/com/hello/hello_store_service/SecurityConfig.kt
@@ -2,20 +2,22 @@ package com.hello.hello_store_service
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig {
 
     @Bean
-    fun userDetailsService(): InMemoryUserDetailsManager {
-        val user: UserDetails = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("meme")
-            .roles("USER")
-            .build()
-        return InMemoryUserDetailsManager(user)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { auth -> auth.anyRequest().permitAll() } // 所有请求都允许
+            .csrf { it.disable() } // 如果需要测试 POST 请求
+
+        return http.build()
     }
 }
