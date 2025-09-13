3b4877a1ba1e4a5bd84a4a2e9343d5ad29e6f8c1
boon <qxxpdada@outlook.com>
2025-09-13 15:16:12 +0800

    feat(GoodsController): 新增商品接口支持多图上传
    修改商品创建接口，从JSON请求体改为表单多文件上传，支持主图、轮播图和详情图的上传处理。

--- src/main/kotlin/com/hello/hello_store_service/controller/GoodsController.kt
+++ src/main/kotlin/com/hello/hello_store_service/controller/GoodsController.kt
@@ -1,6 +1,7 @@
 package com.hello.hello_store_service.controller
 
 import com.hello.hello_store_service.model.Goods
+import com.hello.hello_store_service.service.FileService
 import com.hello.hello_store_service.service.GoodsService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 
 @RestController
 @RequestMapping("/goods")
 class GoodsController(
-    private val goodsService: GoodsService
+    private val goodsService: GoodsService,
+    private val fileService: FileService,
 ) {
 
     // 查询所有商品
@@ -38,8 +41,45 @@ class GoodsController(
     }
 
     // 新增商品
-    @PostMapping
-    fun createGoods(@RequestBody goods: Goods): Goods {
+    @PostMapping("/create")
+    fun createGoods(
+        @RequestParam("title") title: String,
+        @RequestParam("primaryImage") primaryImage: MultipartFile,
+        @RequestParam("images") images: List<MultipartFile>,
+        @RequestParam("desc") desc: List<MultipartFile>
+    ): Goods {
+        // 上传主图
+        val primaryUrl = fileService.uploadFile(primaryImage)
+
+        // 上传轮播图
+        val imagesUrl = images.map { fileService.uploadFile(it) }
+
+        // 上传详情图
+        val descUrl = desc.map { fileService.uploadFile(it) }
+
+        val goods = Goods(
+            saasId = "88888888",
+            storeId = "1000",
+            spuId = "0",
+            title = title,
+            primaryImage = primaryUrl,
+            images = imagesUrl,
+            video = null,
+            minSalePrice = 1000,
+            minLinePrice = 1200,
+            maxSalePrice = 2000,
+            maxLinePrice = 2200,
+            spuStockQuantity = 100,
+            soldNum = 0,
+            isPutOnSale = 1,
+            specList = emptyList(),
+            skuList = emptyList(),
+            spuTagList = emptyList(),
+            limitInfo = null,
+            desc = descUrl,
+            etitle = ""
+        )
+
         return goodsService.createGoods(goods)
     }
 
