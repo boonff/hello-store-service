ae695125a633c315671c179fd6b9e4d573ea82a3
boon <qxxpdada@outlook.com>
2025-09-14 20:57:33 +0800

    feat(CommentCount):添加数据传输对象

---
+++ src/main/kotlin/com/hello/hello_store_service/model/dto/CommentCount.kt
@@ -0,0 +1,11 @@
+package com.hello.hello_store_service.model.dto
+
+data class CommentCount(
+    val commentCount: Int,
+    val badCount: Int,
+    val middleCount: Int,
+    val goodCount: Int,
+    val hasImageCount: Int,
+    val goodRate: Float,
+    val uidCount: Int
+)
