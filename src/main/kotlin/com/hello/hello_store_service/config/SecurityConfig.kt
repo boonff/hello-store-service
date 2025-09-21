29b789fd3afe5c50e7c1eb84ea33cb39a49a8cd2
boon <qxxpdada@outlook.com>
2025-09-21 17:53:12 +0800

    feat(security): 添加API配置端点免认证访问

--- src/main/kotlin/com/hello/hello_store_service/config/SecurityConfig.kt
+++ src/main/kotlin/com/hello/hello_store_service/config/SecurityConfig.kt
@@ -15,13 +15,18 @@ class SecurityConfig {
         http
             .csrf { it.disable() }
             .authorizeHttpRequests { auth ->
-                auth.requestMatchers("/auth/**", "/goods/**", "/comments/**").permitAll() // 登录注册放行
+                auth.requestMatchers(
+                    "/auth/**",
+                    "/goods/**",
+                    "/comments/**",
+                    "api/config/**",
+                    "api/config/customerService"
+                ).permitAll() // 登录注册放行
                     .anyRequest().authenticated() // 其他接口需要认证
             }
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
     }
-
 
 
 }
