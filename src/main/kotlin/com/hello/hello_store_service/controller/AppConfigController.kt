399fcd4404c8235acf7d01f990d2a1f09c211277
boon <qxxpdada@outlook.com>
2025-09-21 17:53:33 +0800

    feat: 新增客服配置接口

--- src/main/kotlin/com/hello/hello_store_service/controller/AppConfigController.kt
+++ src/main/kotlin/com/hello/hello_store_service/controller/AppConfigController.kt
@@ -1,6 +1,7 @@
 package com.hello.hello_store_service.controller
 
 import com.hello.hello_store_service.model.entity.AppConfig
+import com.hello.hello_store_service.model.entity.CustomerService
 import com.hello.hello_store_service.service.AppConfigService
 import org.springframework.web.bind.annotation.*
 
@@ -20,4 +21,13 @@ class AppConfigController(
     @PostMapping("/init")
     fun initConfig(@RequestBody defaultConfig: AppConfig): AppConfig =
         appConfigService.initConfig(defaultConfig)
+
+    @GetMapping("/customerService")
+    fun getCustomerService(): CustomerService? =
+        appConfigService.getCustomerService()
+
+
+    @PostMapping("/customerService")
+    fun updateCustomerService(@RequestParam config: CustomerService): AppConfig =
+        appConfigService.updateCustomerService(config)
 }
--- src/main/kotlin/com/hello/hello_store_service/service/AppConfigService.kt
+++ src/main/kotlin/com/hello/hello_store_service/service/AppConfigService.kt
@@ -1,19 +1,35 @@
 package com.hello.hello_store_service.service
 
 import com.hello.hello_store_service.model.entity.AppConfig
+import com.hello.hello_store_service.model.entity.CustomerService
 import com.hello.hello_store_service.repository.AppConfigRepository
 import org.springframework.stereotype.Service
+import java.util.logging.Logger
 
 @Service
 class AppConfigService(
-    private val repository: AppConfigRepository
+    private val repository: AppConfigRepository,
 ) {
     fun getConfig(): AppConfig? =
-        repository.findById("appConfig").orElse(null)
+        repository.findByConfigId("appConfig")
 
     fun updateConfig(config: AppConfig): AppConfig =
-        repository.save(config.copy(id = "appConfig"))
+        repository.save(config.copy(configId = "appConfig"))
 
     fun initConfig(defaultConfig: AppConfig): AppConfig =
-        getConfig() ?: repository.save(defaultConfig.copy(id = "appConfig"))
+        getConfig() ?: repository.save(defaultConfig.copy(configId = "appConfig"))
+
+    /**
+    * 获取客服配置
+     */
+    fun getCustomerService(): CustomerService? =
+        getConfig()?.customerService
+
+    /**
+     * 更新客服配置
+     */
+    fun updateCustomerService(customerService: CustomerService): AppConfig {
+        val config = getConfig() ?: AppConfig(customerService = customerService)
+        return repository.save(config.copy(customerService = customerService))
+    }
 }
