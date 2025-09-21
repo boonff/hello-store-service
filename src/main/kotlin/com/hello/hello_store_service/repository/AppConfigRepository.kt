6f926a69f8010903a3e5ecc46ba921bf3500d53e
boon <qxxpdada@outlook.com>
2025-09-21 15:14:54 +0800

    feat(repository): 添加AppConfigRepository接口

---
+++ src/main/kotlin/com/hello/hello_store_service/repository/AppConfigRepository.kt
@@ -0,0 +1,8 @@
+package com.hello.hello_store_service.repository
+
+import com.hello.hello_store_service.model.entity.AppConfig
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AppConfigRepository : MongoRepository<AppConfig, String>
