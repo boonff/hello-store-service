c707d44c45fada0f5a90d69fddb5687320bc8bf1
boon <qxxpdada@outlook.com>
2025-10-07 17:39:01 +0800

    feat(entity): 在Spu实体中调整商品描述字段位置
    调整desc字段位置以优化实体类结构

--- src/main/kotlin/com/hello/hello_store_service/model/entity/goods/Spu.kt
+++ src/main/kotlin/com/hello/hello_store_service/model/entity/goods/Spu.kt
@@ -10,15 +10,19 @@ data class Spu(
     val spuId: String,                       // SPU ID
     val saasId: String,                      // SaaS 平台 ID
     val storeId: String,                     // 店铺 ID
+
     val title: String,                       // 商品标题
     val etitle: String,                      // 英文标题
     val primaryImage: String,                // 主图
     val images: List<String>,                // 轮播图
     val video: String? = null,               // 视频
+    val desc: List<String> = emptyList(),         // 商品描述（图文详情）
+
     val categoryIds: List<String>? = emptyList(), // 分类
     val groupIdList: List<String>? = emptyList(), // 分组
     val spuTagList: List<SpuTag> = emptyList(),   // 标签
-    val desc: List<String> = emptyList(),         // 商品描述（图文详情）
+
+
     val available: Int? = null,               // 是否可售
     val minSalePrice: Int? = null,            // 最低参考售价
     val maxSalePrice: Int? = null,            // 最高参考售价
